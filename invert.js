a126c524e3037158628bb189c9ba28d59e409b7e
Julia Guo <jzg2110@columbia.edu>
2020-06-18 12:44:24 -0400

    flesh out night mode :D

--- invert.js
+++ invert.js
@@ -1,14 +1,17 @@
 var white = 1;
+var body = document.getElementsByTagName('body')[0];
 
 function invert(){
 	if(white){
-		document.documentElement.style.setProperty('--bg', '#151515');
-		document.documentElement.style.setProperty('--bg2', '#fff');
+		document.documentElement.style.setProperty('--white', '#151515');
+		document.documentElement.style.setProperty('--black', '#fff');
+		body.style.backgroundImage = 'url(low.png)';
 		white = 0;
 	}
 	else{
-		document.documentElement.style.setProperty('--bg', '#fff');
-		document.documentElement.style.setProperty('--bg2', '#aaa');
+		document.documentElement.style.setProperty('--white', '#fff');
+		document.documentElement.style.setProperty('--black', '#151515');
+		body.style.backgroundImage = 'url(grid.png)';
 		white = 1;
 	}
 }
